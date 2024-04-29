3173dbac002df3a6dc0fd0ed55cd5e763543e41c
Gabxiel30 <108111815+GrystanDev@users.noreply.github.com>
2024-04-29 12:14:42 -0500

    ShadcnInstalled

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import TaskForm from "./components/TaskForm";
+
 function App() {
   return (
     <>
-      
+      <div className="container mx-auto p-10">
+        <TaskForm />
+      </div>
     </>
   );
 }
