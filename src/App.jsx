dc5f11d0d0dc2d39969765d221f736050cbdb20b
Gabxiel30 <108111815+GrystanDev@users.noreply.github.com>
2024-05-06 20:26:06 -0500

    rename icon.svg

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,6 @@ function App() {
     <>
       <div className="flex justify-end p-5">
         <ModeToggle />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="h-6 w-6 text-blue-600"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-          />
-        </svg>
       </div>
       <TaskForm />
       <Separator className="mt-8" />
